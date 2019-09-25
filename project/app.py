e5a020f4d555ab511851024eb91a87297181addf
simonstumpf <32813906+simonstumpf@users.noreply.github.com>
2019-09-25 15:58:09 +0200

    Update app.py

--- project/app.py
+++ project/app.py
@@ -1,1 +1,9 @@
-print("hello")
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    application.run()
